20848494695dee2bc9029017292b9e9d250eb166
abdelrahman-saher <abdelrahman.saher@gmail.com>
2022-09-21 03:53:04 +0200

    define people repository

---
+++ lib/features/popular_people/domain/repository/people_repository.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:themovie/features/popular_people/domain/entities/people_container.dart';
+
+import '../../../../core/error/failures.dart';
+
+abstract class NotificationsRepository {
+  Future<Either<Failure, PeopleContainer>> getPeople({int page = 0});
+}
