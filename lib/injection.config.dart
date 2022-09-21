200d30025f22f07981be44810bccaeb0458592e9
abdelrahman-saher <abdelrahman.saher@gmail.com>
2022-09-21 02:25:56 +0200

    add injection modules

--- lib/injection.config.dart
+++ lib/injection.config.dart
@@ -5,9 +5,12 @@
 // **************************************************************************
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:dio/dio.dart' as _i3;
 import 'package:get_it/get_it.dart' as _i1;
-import 'package:injectable/injectable.dart'
-    as _i2; // ignore_for_file: unnecessary_lambdas
+import 'package:injectable/injectable.dart' as _i2;
+
+import 'core/injection/injectable_modules.dart'
+    as _i4; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
@@ -21,5 +24,9 @@ _i1.GetIt $initGetIt(
     environment,
     environmentFilter,
   );
+  final injectableModule = _$InjectableModule();
+  gh.singleton<_i3.Dio>(injectableModule.dio);
   return get;
 }
+
+class _$InjectableModule extends _i4.InjectableModule {}
