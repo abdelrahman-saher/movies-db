815ecd3b5bfb5462f6ddd9f7da9b132e82063b18
abdelrahman-saher <abdelrahman.saher@gmail.com>
2022-09-21 07:01:45 +0200

    declare get images

--- lib/features/person_details/domain/repository/person_info_repository.dart
+++ lib/features/person_details/domain/repository/person_info_repository.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:themovie/features/person_details/domain/entities/person_image.dart';
 import 'package:themovie/features/person_details/domain/entities/person_info.dart';
 import 'package:themovie/features/popular_people/domain/entities/person.dart';
 
@@ -6,4 +7,6 @@ import '../../../../core/error/failures.dart';
 
 abstract class PersonInfoRepository {
   Future<Either<Failure, PersonInfoEntity>> getInfo({PersonEntity? person});
+  Future<Either<Failure, List<PersonImageEntity>>> getImages(
+      {PersonEntity? person});
 }
