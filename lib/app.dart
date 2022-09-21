28dbbaeeabe0eba2ecfcd7505e8be1318c4fceb0
abdelrahman-saher <abdelrahman.saher@gmail.com>
2022-09-21 02:24:06 +0200

    add app router

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'config/routes/router.gr.dart';
+
 class AppWidget extends StatelessWidget {
+  final _appRouter = AppRouter();
   AppWidget({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: "Movies App",
       theme: ThemeData(
         backgroundColor: Colors.white,
       ),
-      home: Scaffold(
-        body: Container(),
-      ),
+      routerDelegate: _appRouter.delegate(),
+      routeInformationParser: _appRouter.defaultRouteParser(),
     );
   }
 }
