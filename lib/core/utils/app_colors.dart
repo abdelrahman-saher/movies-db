37c0a8bdd26115cd169ca6a0f74c7d3d1920039c
abdelrahman-saher <abdelrahman.saher@gmail.com>
2022-09-21 05:34:21 +0200

    change loading indicator color

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -6,4 +6,5 @@ class AppColors {
   static Color background = HexColor('#ffffff');
   static Color dark = HexColor('#0d253f');
   static Color light = HexColor('#a0a1a3');
+  static Color lightGreen = HexColor('#90cea1');
 }
--- lib/core/utils/shared_components.dart
+++ lib/core/utils/shared_components.dart
@@ -3,6 +3,6 @@ import 'package:themovie/core/utils/app_colors.dart';
 
 class AppComponents {
   static Widget load = CircularProgressIndicator(
-    color: AppColors.dark,
+    color: AppColors.lightGreen,
   );
 }
